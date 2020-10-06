97aa42c1133e573ada52bb4c7931014e9d7c5788
inizio <newman.kcchow@gmail.com>
2020-10-07 01:46:43 +0800

    feat(PWA): Add PWA manifest and uselss service worker

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', event => {
+
+  // function onInstall () {
+  //   return caches.open('static')
+  //     .then(cache => cache.addAll([
+  //       '/'
+  //     ])
+  //   );
+  // }
+
+  // event.waitUntil(onInstall(event));
+});
